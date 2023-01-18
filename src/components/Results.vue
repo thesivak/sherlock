fae5c420cba69320eece9d2eaacaea2b1b2c2fcd
Mirek Sivák <miroslav.sivak@memsource.com>
2023-01-18 11:58:46 +0100

    wip

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -9,7 +9,9 @@ export default defineComponent({
 <template>
 <main class="h-full p-6">
   <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-    <Card/>
+    <Card resource-name="Connectors" results-count="700"/>
+    <Card resource-name="LogzIO" results-count="346"/>
+    <Card resource-name="APCs" results-count="52"/>
   </ul>
 
 </main>
