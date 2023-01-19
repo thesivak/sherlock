ee123cea77a0b2c7da19df1cc9f5cf45d9bdc319
davidbarta <davidbarta2@gmail.com>
2023-01-19 10:02:53 +0100

    Updated API functions; implemented search handling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
 import Search from './components/Search.vue'
 import Results from './components/Results.vue';
+import { checkStatus, searchByQuery } from './helpers/api';
+import { ref } from 'vue';
 
-
-const handleSearch = (query: string) => {
-    // parse
-}
+const resultsData = ref([]);
+const handleSearch = async (searchQuery: string) => {
+    let idsArray: string[] = [];
+    await searchByQuery(searchQuery)
+        .then(data => {
+            resultsData.value = [];
+            idsArray = data
+        });
+    for (const id of idsArray) {
+        await checkStatus(id).then(response => {
+            if (response.data.status === 'FOUND') {
+                resultsData.value.push(response.data);
+            } else if (response.data.status === 'PENDING') {
+                let counter = 0;
+                const repeatCheckId = setInterval(async function repeatCheck() {
+                    if (counter === 10) {
+                        clearInterval(repeatCheckId);
+                        return;
+                    }
+                    counter++;
+                    await checkStatus(id)
+                        .then(response => {
+                            if (response.data.status === 'FOUND') {
+                                resultsData.value.push(response.data);
+                            }
+                        });
+                }, 2000);
+            }
+        });
+    }
+};
 </script>
 
 <template>
--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { CHECK_STATUS_URL, SEARCH_BY_INPUT_URL } from '../constants/constants';
 
-const searchByQuery = (name: string) => {
-    axios.post(SEARCH_BY_INPUT_URL + '?input=' + name)
-        .then(response => response);
+const searchByQuery = (name: string): Promise<Array<string>> => {
+    return axios.get(SEARCH_BY_INPUT_URL + '?input=' + name)
+        .then(response => response.data as Array<string>);
 }
 
-const checkStatus = (name: string) => {
-    axios.get(CHECK_STATUS_URL + '?record=' + name)
+const checkStatus = (name: string): Promise<AxiosResponse> => {
+    return axios.get(CHECK_STATUS_URL + '?record=' + name)
         .then(response => response);
 }
 
